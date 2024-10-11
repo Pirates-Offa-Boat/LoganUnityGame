4e09b371ab60a4b75796f0d1b639225526f290af
Little Chungus <loganmccandless8@gmail.com>
2024-10-10 23:49:47 -0400

    Fixed the ball movement

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class MoveBall : MonoBehaviour
 {
-
     public Rigidbody rb;
-    // Start is called before the first frame update
+    public float moveSpeed = 10f;
+    public float jumpForce = 15f;
+    public bool isGrounded;
+    public float gravityMultiplier = 2f; // Custom gravity multiplier
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-            rb.position += Vector3.up/65;
+        // Horizontal movement (X-axis)
+        float moveX = 0;
         if (Input.GetKey(KeyCode.A))
-            rb.position += Vector3.left/75;
+            moveX = -1;
         if (Input.GetKey(KeyCode.D))
-            rb.position += Vector3.right/75;
+            moveX = 1;
+
+        Vector3 movement = new Vector3(moveX * moveSpeed, rb.velocity.y, 0);
+        rb.velocity = movement;
+
+        // Jumping (Y-axis)
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false; // Prevent double jump
+        }
+
+        // Apply extra gravity when not grounded
+        if (!isGrounded)
+        {
+            rb.AddForce(Vector3.down * gravityMultiplier, ForceMode.Acceleration);
+        }
+    }
+
+    // Detect ground collision to enable jumping again
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts[0].normal.y > 0.5f) // Check if landing on a surface
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false; // When the ball leaves the ground
     }
 }
