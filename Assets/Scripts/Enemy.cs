ca3b9e0fe0980edea0379f46d22bd10310b80ce7
Little Chungus <loganmccandless8@gmail.com>
2024-11-07 17:16:32 -0500

    Made enemy movement to go between waypoints

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Waypoints Wpoints;
+
+    private int waypointIndex;
+
+    void Start()
+    {
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    void Update()
+    {
+        // Use Vector3.MoveTowards to move along the x, y, and z axes.
+        transform.position = Vector3.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        // Check if the enemy has reached the current waypoint.
+        if (Vector3.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            // Increment the waypoint index to move to the next waypoint.
+            waypointIndex++;
+        }
+    }
+}
